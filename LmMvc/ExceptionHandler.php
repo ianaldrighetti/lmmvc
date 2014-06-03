accb3f21bd2a9e112cca8c605dcffeb4dee9d076
Ian Aldrighetti <ian.aldrighetti@adp.com>
2014-06-03 16:22:35 -0700

    I renamed the run method to setup, which returns an array containing an instance of the controller, the controller name and the arguments to invoke the method with. Then I made a new run method which is used to isolate the internally generated exceptions from those that might be thrown (and uncaught) by the application itself. Either way, these are caught and handled by the exception handler. If none is set, the default will be used (not made yet).

---
+++ LmMvc/ExceptionHandler.php
@@ -0,0 +1,29 @@
+<?php
+namespace LmMvc;
+
+/**
+ * Interface ExceptionHandler
+ *
+ * If you wish to implement a custom exception handler, you must implement this interface.
+ *
+ * @package LmMvc\Exception
+ */
+interface ExceptionHandler
+{
+    /**
+     * This is the only method that an Exception Handler must implement. It will be passed the exception instance and
+     * whether the exception was internal (to LMMVC). There are a few exceptions you can expect LMMVC to throw one time
+     * or another:
+     *      ControllerException - Thrown if a controller does not implement BaseController.
+     *      MalformedUriException - Thrown if the URI was malformed and could not be processed.
+     *      PageNotFound - Thrown if the page was not found (as in, the controller could not be found, the method
+     *                     doesn't exist, etc.).
+     *
+     * @param \Exception $ex The exception thrown.
+     * @param bool $internal If true it means that the exception was generated internally (by LMMVC), otherwise (false)
+     *                       it means that the exception was thrown by a controller (so probably the application
+     *                       itself).
+     * @return void
+     */
+    public function handle(\Exception $ex, $internal = true);
+}
